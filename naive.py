c860675680b4a5d519614378126ea909ddc2d8db
Edgar García Rodríguez <edgarciarod@gmail.com>
2015-10-27 06:46:06 -0600

    Added the deque class

--- naive.py
+++ naive.py
@@ -1,24 +1,26 @@
-# Author:   Austin Walters
+# Author:   Austin Walters and Edgar García Rodríguez
 # Language: Python 3.5.0 
 
 import sys
 import time
+from collections import deque
 
 # Run through round of game
 def round(deck):
-    table = []
+            
+    table = deque()
     while len(deck) > 0:
-        table.append(deck.pop(0))
+        table.append(deck.popleft())
         if len(deck) != 0:
-            deck.append(deck.pop(0))
+            deck.append(deck.popleft())
     table.reverse()
     return table                                                            
 
 # Run through game
 def game(count):
     
-    deck = []
-    origDeck = []
+    deck = deque()
+    origDeck = deque()
     ans = 1
 
     # Generate deck and copy
@@ -31,10 +33,14 @@ def game(count):
 
     # Loop through until deck is life first
     while deck != origDeck:
+
         deck = round(deck)
         ans += 1
         
     return ans
+
+
+
         
 ''' ___ MAIN ___ '''
 if len(sys.argv) == 2 and int(sys.argv[1]) > 0:
@@ -42,4 +48,3 @@ if len(sys.argv) == 2 and int(sys.argv[1]) > 0:
     print('\n', size, "cards =>", game(size), "rounds\n")
 else:
     print('\n', "Invalid, should be in the form:\n\n\t$ python naive.py (deck size > 0)\n")
-        
